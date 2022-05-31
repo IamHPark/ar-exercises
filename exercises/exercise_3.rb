78327eeaa7d7b827b6cef6c4eb3902b3ae0b2189
IamHPark <heesoo.p1023@gmail.com>
2022-05-31 03:40:48 +0000

    delete data

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# load third data
+@store3 = Store.find_by(id: 3)
+
+# delete data
+@store3.destroy
+
+# count num of data
+puts Store.count
