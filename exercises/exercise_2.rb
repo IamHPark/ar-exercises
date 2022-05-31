3256b3bc76cdb4d9ef76ea6c7d2c04215af64c30
IamHPark <heesoo.p1023@gmail.com>
2022-05-31 03:38:36 +0000

    load data and update first data

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+#load the first store and assign as @store1
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Coquitlam")
